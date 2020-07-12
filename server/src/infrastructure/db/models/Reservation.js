f169a1e1271e5f705f7b271dd487126cae1ad33a
larry adams <ladams1776@yahoo.com>
2020-07-12 00:05:12 -0400

    Apply prettier to the mongoose model (Reservation)

--- server/src/infrastructure/db/models/Reservation.js
+++ server/src/infrastructure/db/models/Reservation.js
@@ -1,24 +1,23 @@
 const mongoose = require('mongoose');
 
-
 const ReservationSchema = new mongoose.Schema({
-    id: { type: Number, index: true },
-    name: {
-        type: String
-    },
-    url: {
-        type: String,
-        trim: true
-    },
-    partySize: {
-        type: Number
-    },
-    dates: {
-        type: [String]
-    },
-    times: {
-        type: [String]
-    }
+  id: { type: Number, index: true },
+  name: {
+    type: String,
+  },
+  url: {
+    type: String,
+    trim: true,
+  },
+  partySize: {
+    type: Number,
+  },
+  dates: {
+    type: [String],
+  },
+  times: {
+    type: [String],
+  },
 });
 
 const Reservation = mongoose.model('reservations', ReservationSchema);

67a83bbc5ffb80902277ac84a47aebcc221802eb
larry adams <ladams1776@yahoo.com>
2020-07-12 00:03:29 -0400

    apply prettier to the action

--- server/src/application/requestHandlers/__test__/getAllReservations.test.js
+++ server/src/application/requestHandlers/__test__/getAllReservations.test.js
@@ -4,24 +4,26 @@ const getAllReservations = require('../getAllReservations');
 jest.mock('../../../domain/ReservationService');
 
 describe('server/src/application/requestHandlers/__test__/getAllReservations.test.js', () => {
-    describe('getAllReservations', () => {
-        it('should call status 200 and jsonp with reservations', async () => {
-            // Arrange
-            const req = {};
-            const res = {
-                status: jest.fn().mockImplementation(() => res),
-                jsonp: jest.fn(),
-            };
+  describe('getAllReservations', () => {
+    it('should call status 200 and jsonp with reservations', async () => {
+      // Arrange
+      const req = {};
+      const res = {
+        status: jest.fn().mockImplementation(() => res),
+        jsonp: jest.fn(),
+      };
 
-            const expected = { id: 1 };
-            jest.spyOn(ReservationService, 'getAllReservations').mockImplementation(() => expected);
+      const expected = { id: 1 };
+      jest
+        .spyOn(ReservationService, 'getAllReservations')
+        .mockImplementation(() => expected);
 
-            // Act
-            await getAllReservations(req, res);
+      // Act
+      await getAllReservations(req, res);
 
-            // Assert
-            expect(res.status).toBeCalledWith(200);
-            expect(res.jsonp).toBeCalledWith({ items: expected });
-        });
+      // Assert
+      expect(res.status).toBeCalledWith(200);
+      expect(res.jsonp).toBeCalledWith({ items: expected });
     });
+  });
 });
--- server/src/application/requestHandlers/getAllReservations.js
+++ server/src/application/requestHandlers/getAllReservations.js
@@ -1,8 +1,8 @@
 const ReservationService = require('../../domain/ReservationService');
 
 const getAllReservations = async (req, res) => {
-    const reservations = await ReservationService.getAllReservations();
-    res.status(200).jsonp({ items: reservations });
-}
+  const reservations = await ReservationService.getAllReservations();
+  res.status(200).jsonp({ items: reservations });
+};
 
 module.exports = getAllReservations;

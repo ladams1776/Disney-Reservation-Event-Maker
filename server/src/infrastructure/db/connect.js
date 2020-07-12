353aa4bf73ffc06457275beeec6287e40c3e8763
larry adams <ladams1776@yahoo.com>
2020-07-12 00:04:55 -0400

    Apply prettier to connect.js

--- server/src/infrastructure/db/connect.js
+++ server/src/infrastructure/db/connect.js
@@ -4,33 +4,32 @@ const mongoose = require('mongoose');
 const SERVER_AND_PORT = `${process.env.dbUser}:${process.env.dbPassword}@${process.env.dbUrl}:${process.env.dbPort}`;
 
 const config = {
-    db: `mongodb://${SERVER_AND_PORT}`,
-    opts: {
-        reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-        reconnectInterval: 5000,
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    },
+  db: `mongodb://${SERVER_AND_PORT}`,
+  opts: {
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 5000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
 };
 
 const connectWithRetry = () => {
-    console.log('MongoDB connection with retry');
-    return mongoose.connect(config.db, config.opts);
+  console.log('MongoDB connection with retry');
+  return mongoose.connect(config.db, config.opts);
 };
 
 mongoose
-    .connect(config.db, config.opts)
-    .catch(() => setTimeout(connectWithRetry, 5000));
+  .connect(config.db, config.opts)
+  .catch(() => setTimeout(connectWithRetry, 5000));
 
 mongoose.Promise = global.Promise;
 // @TODO: Need to auto set data
-mongoose
-    .connection
-    .on('connected', () => {
-        console.log(
-            `Mongoose connection open on mongodb://${SERVER_AND_PORT}/reservations`,
-        );
-    })
-    .on('error', err => {
-        console.log(`Connection error: ${err.message}`);
-    });
+mongoose.connection
+  .on('connected', () => {
+    console.log(
+      `Mongoose connection open on mongodb://${SERVER_AND_PORT}/reservations`
+    );
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });

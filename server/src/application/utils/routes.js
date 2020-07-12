781af2b5fcb4918025add889872a5b00b6e1541e
larry adams <ladams1776@yahoo.com>
2020-07-12 00:04:18 -0400

    apply prettier to errors and routes

--- server/src/application/utils/routes.js
+++ server/src/application/utils/routes.js
@@ -1,11 +1,14 @@
 const getAllReservations = require('../requestHandlers/getAllReservations');
 
 const routes = (app) => {
-    app.get('/status', (req, res) => { res.status(200).jsonp({ success: true }).end(); });
-    app.head('/status', (req, res) => { res.status(200).end(); });
+  app.get('/status', (req, res) => {
+    res.status(200).jsonp({ success: true }).end();
+  });
+  app.head('/status', (req, res) => {
+    res.status(200).end();
+  });
 
-    app.get('/reservations', getAllReservations);
-}
-
+  app.get('/reservations', getAllReservations);
+};
 
 module.exports = routes;

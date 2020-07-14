a5eaf2bdaaa4bb6a2bcd418092596033b2a7c43c
larry adams <ladams1776@yahoo.com>
2020-07-13 23:02:58 -0400

    Begin to add the Reservation Form pieces/style

--- client/src/pages/home/form/AddReservationForm.js
+++ client/src/pages/home/form/AddReservationForm.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import styles from './AddReservationForm.module.css';
 
-const AddReservationForm = () => (
-  <div className={styles.form}>Reservation Form</div>
-);
+const AddReservationForm = () => <div className={styles.form}>Reservation Form
+    <form action="post" data-testid="form">
+    <input name="name" type="text" />
+    <input name="" type="text" />
+  </form>
+</div>
 
 export default AddReservationForm;
--- client/src/pages/home/form/__test__/AddReservationForm.test.js
+++ client/src/pages/home/form/__test__/AddReservationForm.test.js
@@ -1,19 +1,13 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import AddReservationForm from '../AddReservationForm';
+import React from "react";
+import { render } from "@testing-library/react";
+import AddReservationForm from "../AddReservationForm";
 
-test('should render AddReservationForm', () => {
+test("should render AddReservationForm", () => {
   // Arrange
-  const { container } = render(<AddReservationForm />);
+  const { getByTestId } = render(<AddReservationForm />);
 
   // Act
 
   // Assert
-  expect(container.firstChild).toMatchInlineSnapshot(`
-    <div
-      class="form"
-    >
-      Reservation Form
-    </div>
-  `);
+  expect(getByTestId("form"));
 });

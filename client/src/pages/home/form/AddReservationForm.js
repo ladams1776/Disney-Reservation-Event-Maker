9ea9d3e0900be47a337288f9f235cef0b50a63c4
larry adams <ladams1776@yahoo.com>
2020-07-14 23:22:01 -0400

    Update AddReservationForm with more pieces

--- client/src/pages/home/form/AddReservationForm.js
+++ client/src/pages/home/form/AddReservationForm.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import DatePicker from 'components/DatePicker';
+import cn from 'classname';
 import styles from './AddReservationForm.module.css';
+import TimeSelect from './TimeSelect/TimeSelect';
 
-const AddReservationForm = () => (
-  <div className={styles.form}>
-    Reservation Form
-    <form action="post" data-testid="form">
-      <input name="name" type="text" />
-      <input name="" type="text" />
-    </form>
-  </div>
-);
+const AddReservationForm = () => <div className={styles.form} data-testid="form">
+  <form action="post" className={styles.form}>
+    <div className={styles.name}><input name="name" type="text" placeholder="name of reservation" className={styles.input} /><label className={styles.label}>Name</label></div>
+    <div className={styles.url}><input name="url" type="text" placeholder="url" className={styles.input} /><label className={styles.label}>Url</label></div>
+    <div className={styles.partySize}><input name="partySize" type="number" placeholder="partySize" defaultValue="3" className={styles.input} /><label className={styles.label}>Party Size</label></div>
+    <div className={styles.select}><TimeSelect className={styles.input} /><label className={styles.label}>Times</label></div>
+    <div className={styles.date}>
+      <DatePicker />
+      <label className={styles.label}>Dates</label>
+    </div>
+    <input type="submit" value="Submit" className={styles.submit} />
+  </form>
+</div>
 
 export default AddReservationForm;

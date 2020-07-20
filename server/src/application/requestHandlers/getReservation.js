76fd7e44fee40801dc6201c894bb084fcb3f126e
larry adams <ladams1776@yahoo.com>
2020-07-19 22:24:54 -0400

    Clean up the getReservation RH

--- server/src/application/requestHandlers/getReservation.js
+++ server/src/application/requestHandlers/getReservation.js
@@ -1,6 +1,6 @@
 const ReservationService = require('../../domain/ReservationService');
 
-module.exports = async (req, res) => {
+module.export = async (req, res) => {
     const reservation = await ReservationService.getReservation(req.params.id);
     res.status(200).jsonp({ items: reservation });
 };

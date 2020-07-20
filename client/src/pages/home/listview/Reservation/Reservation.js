6419d7d32122aadce8437d36853babbc8486a41a
larry adams <ladams1776@yahoo.com>
2020-07-19 22:23:57 -0400

    Add onclick functionality to Reservation

--- client/src/pages/home/listview/Reservation/Reservation.js
+++ client/src/pages/home/listview/Reservation/Reservation.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import styles from './Reservation.module.css';
+import { useHistory } from 'react-router-dom';
 
-const Reservation = ({ name, time, startDate, endDate }) => {
+const Reservation = ({ _id, name, time, startDate, endDate }) => {
+  const { push } = useHistory();
+
+  const onClick = event => {
+    event.preventDefault();
+    push(`/${_id}`);
+  };
+
   return (
-    <div className={styles.reservation}>
+    <div className={styles.reservation} onClick={onClick}>
       <div className={styles.title}>
         <span className={styles.name}>{name}</span>
       </div>

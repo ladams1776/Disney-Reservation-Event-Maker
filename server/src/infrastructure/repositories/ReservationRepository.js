5e2e55aadf533cb3fca86f220896e24f6d723f34
larry adams <ladams1776@yahoo.com>
2020-07-23 07:35:13 -0400

    Make extra method in Repo to handle updates

--- server/src/infrastructure/repositories/ReservationRepository.js
+++ server/src/infrastructure/repositories/ReservationRepository.js
@@ -1,10 +1,30 @@
-const { ObjecId } = require('mongoose');
 const Reservation = require('../db/models/Reservation');
 
 const ReservationRepository = {
   getAllReservations: () => Reservation.find({}),
   addReservation: reservation => Reservation.create(reservation),
+  updateReservation: (doc, response) => {
+    console.log('Repo: ', doc);
+    Reservation.findOneAndUpdate({ _id: doc._id }, {
+      $set: {
+        endDate: doc.endDate,
+        name: doc.name,
+        partySize: doc.partySize,
+        startDate: doc.startDate,
+        time: doc.time,
+        url: doc.url,
+      },
+    },
+      { new: true, useFindAndModify: false },
+      (err, item) => {
+        if (err) throw err;
+        console.log('item is: ', item);
+        response(item)
+      });
+  },
   getReservation: id => Reservation.find({ _id: id })
 };
 
 module.exports = ReservationRepository;
+
+

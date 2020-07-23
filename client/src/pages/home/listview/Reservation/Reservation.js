4f7be4fc93d79bf1bad3cb6bca08f12dd38ea9ae
larry adams <ladams1776@yahoo.com>
2020-07-23 07:27:28 -0400

    Update Reservation in LV to show only name and styled

--- client/src/pages/home/listview/Reservation/Reservation.js
+++ client/src/pages/home/listview/Reservation/Reservation.js
@@ -12,15 +12,12 @@ const Reservation = ({ _id, name, time, startDate, endDate, isSelected }) => {
   };
 
   return (
-    <div className={cn(styles.reservation, { [styles.isSelected]: isSelected })} onClick={onClick} >
+    <div
+      className={cn(styles.reservation, { [styles.isSelected]: isSelected })}
+      onClick={onClick}
+    >
       <div className={styles.title}>
         <span className={styles.name}>{name}</span>
-      </div>
-      <div className={styles.subTitle}>
-        <span className={styles.dates}>
-          {startDate}-{endDate}
-        </span>
-        <span className={styles.time}>{time}</span>
       </div>
     </div>
   );

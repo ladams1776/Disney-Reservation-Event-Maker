2cfad833b2bd66844e6a45626452ece7a663a0ef
larry adams <ladams1776@yahoo.com>
2020-07-23 07:35:29 -0400

    Create new method in service to upate reservation

--- server/src/domain/ReservationService.js
+++ server/src/domain/ReservationService.js
@@ -4,6 +4,7 @@ const ReservationService = {
   getAllReservations: () => ReservationRepository.getAllReservations(),
   getReservation: (id, responder) => ReservationRepository.getReservation(id, responder),
   addReservation: (reservation) => ReservationRepository.addReservation(reservation),
+  updateReservation: (reservation, response) => ReservationRepository.updateReservation(reservation, response),
 };
 
 module.exports = ReservationService;

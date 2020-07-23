18670fe44d5c3b8713b7f4afeb749906d219ddb7
larry adams <ladams1776@yahoo.com>
2020-07-23 07:35:41 -0400

    Create an update Action RH

---
+++ server/src/application/requestHandlers/updateReservation.js
@@ -0,0 +1,8 @@
+const ReservationService = require('../../domain/ReservationService');
+
+module.exports = async (req, res) => {
+    const response = (item) => {
+        res.jsonp({ ok: true, items: item });
+    }
+    const resev = await ReservationService.updateReservation(req.body, response);
+};

5909b2a74653a8d928dc318fda380649d467fada
larry adams <ladams1776@yahoo.com>
2020-07-23 07:30:22 -0400

    Fix hook for fetching reservation
    It was firing too many times cuz of the method change. I tried using callback(), but it seemed to fire just as much, so removed the unnecessary call to callback and

--- client/src/pages/home/form/useFetchReservation.js
+++ client/src/pages/home/form/useFetchReservation.js
@@ -1,10 +1,11 @@
-import { useEffect } from 'react';
-import fetchApiData from '../../../utils/fetchApiData';
+import { useEffect } from "react";
+import fetchApiData from "../../../utils/fetchApiData";
 
 const useFetchReservation = (id, dispatch) => {
-  useEffect(() => {
-    (() => fetchApiData(`api/reservation?_id=${id}`, {}, dispatch))();
-  }, [id, dispatch]);
+    useEffect(() => {
+        (() => fetchApiData(`api/reservation?_id=${id}`, {}, dispatch))();
+    }, [id]);
 };
+
 
 export default useFetchReservation;
